98a4cc770d069e780eb12b2d541a5470920b3d3b
rjdanielsofficialIWS <rjdanielsofficial@gmail.com>
2026-01-09 18:30:06 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MetaPixel } from './components/MetaPixel';
+import { MetaPixelTracker } from './components/MetaPixelTracker';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
